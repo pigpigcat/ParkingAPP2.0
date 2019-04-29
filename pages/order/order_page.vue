4223bebfa3285713bce3d4d3e9975f6997d29869
lt734475941 <734475941@qq.com>
2019-04-29 14:21:47 +0800

    order页面

---
+++ pages/order/order_page.vue
@@ -0,0 +1,509 @@
+<template>
+	<view class="content">
+		<view class="navbar">
+			<view 
+				v-for="(item, index) in navList" :key="index" 
+				class="nav-item" 
+				:class="{current: tabCurrentIndex === index}"
+				@click="tabClick(index)"
+			>
+				{{item.text}}
+			</view>
+		</view>
+
+		<swiper :current="tabCurrentIndex" class="swiper-box" duration="300" @change="changeTab">
+			<swiper-item class="tab-content" v-for="(tabItem,tabIndex) in navList" :key="tabIndex">
+				<scroll-view 
+					class="list-scroll-content" 
+					scroll-y>
+					
+					<!-- 订单列表 -->
+					<view 
+						v-for="(item,index) in tabItem.orderList" :key="index"
+						class="order-item">
+						<uni-card :is-full="true" :title="item.parking_name" :extra="item.create_time"
+						thumbnail="https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png"  :note="item.create_time">
+						  <view class="uni-flex uni-row" style="justify-content: space-between;">
+						  	<view class="text"></view>
+						  	<view class="text" :style="{color: item.stateTipColor}">{{item.stateTip}}</view>
+						  </view>
+						 <view class="uni-flex uni-row" style="justify-content: space-between;">
+						 	<view class="text price-box">车位:<text class="parking-place">{{item.parking_place}}</text></view>
+						 	<view class="text price-box" >实付款:<text class="price">{{item.price}}</text></view>
+						 </view>
+						<view class="action-box b-t" v-if="item.state == 1">
+							<button class="action-btn" @click="cancelOrder(item)">取消订单</button>
+							<button class="action-btn recom">立即支付</button>
+						</view>
+						</uni-card>
+						
+						
+						
+					</view>
+					 
+					<uni-load-more :status="tabItem.loadingType"></uni-load-more>
+					
+				</scroll-view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template> 
+
+<script>
+	import uniLoadMore from '@/components/uni-load-more/uni-load-more.vue';
+	import uniCard from '@/components/uni-card/uni-card.vue'
+	const order_List = [{
+		create_time: '2019-04-06 11:37',
+		state: 1,
+		parking_name:'有个停车场',
+		parking_place:'A区88号',
+		price:'150',
+	},
+	{
+		create_time: '2019-04-26 11:37',
+		state: 2,
+		parking_name:'有个停车场',
+		parking_place:'A区89号',
+		price:'100',
+	},{
+		create_time: '2019-04-16 11:37',
+		state: 1,
+		parking_name:'有个停车场',
+		parking_place:'A区11号',
+		price:'120',
+	}];
+	export default {
+		components: {
+			uniLoadMore	,uniCard
+		},
+		data() {
+			return {
+				tabCurrentIndex: 0,
+				orderList: [],
+				navList: [{
+						state: 0,
+						text: '全部',
+						loadingType: 'more',
+						orderList: []
+					},
+					{
+						state: 1,
+						text: '未完成',
+						loadingType: 'more',
+						orderList: []
+					},
+					{
+						state: 2,
+						text: '完成',
+						loadingType: 'more',
+						orderList: []
+					}
+				],
+			};
+		},
+		
+		onLoad(options){
+			
+			// #ifndef MP
+			this.loadData()
+			// #endif
+			
+			
+		},
+		 
+		methods: {
+			//获取订单列表
+			loadData(source){
+				//这里是将订单挂载到tab列表下
+				let index = this.tabCurrentIndex;
+				let navItem = this.navList[index];
+				let state = navItem.state;
+				navItem.orderList.splice(0,navItem.orderList.length); 
+				setTimeout(()=>{
+					//返回Json.order_List中订单状态等于state的List
+					let orderList = order_List.filter(item=>{
+						//添加不同状态下订单的表现形式
+						//orderStateExp，返回订单状态含颜色和文字的对象，与item合并成一个对象
+						item = Object.assign(item, this.orderStateExp(item.state));
+						//演示数据所以自己进行状态筛选
+						if(state === 0){
+							//0为全部订单
+							return item;
+						}
+						return item.state === state
+					});
+					orderList.forEach(item=>{
+						navItem.orderList.push(item);
+					})
+					//loaded新字段用于表示数据加载完毕，如果为空可以显示空白页
+					this.$set(navItem, 'loaded', true);
+					
+					//判断是否还有数据， 有改为 more， 没有改为noMore 
+					navItem.loadingType = 'nomore';
+				}, 600);	
+			}, 
+
+			//swiper 切换
+			changeTab(e){
+				this.tabCurrentIndex = e.target.current;
+				this.loadData('tabChange');
+			},
+			//顶部tab点击
+			tabClick(index){
+				this.tabCurrentIndex = index;
+			},
+			//删除订单
+			deleteOrder(index){
+				uni.showLoading({
+					title: '请稍后'
+				})
+				setTimeout(()=>{
+					this.navList[this.tabCurrentIndex].orderList.splice(index, 1);
+					uni.hideLoading();
+				}, 600)
+			},
+			//取消订单
+			cancelOrder(item){
+				uni.showLoading({
+					title: '请稍后'
+				})
+				setTimeout(()=>{
+					let {stateTip, stateTipColor} = this.orderStateExp(9);
+					item = Object.assign(item, {
+						state: 9,
+						stateTip, 
+						stateTipColor
+					})
+					
+					//取消订单后删除待付款中该项
+					let list = this.navList[1].orderList;
+					let index = list.findIndex(val=>val.id === item.id);
+					index !== -1 && list.splice(index, 1);
+					
+					uni.hideLoading();
+				}, 600)
+			},
+
+			//订单状态文字和颜色
+			orderStateExp(state){
+				let stateTip = '',
+					stateTipColor = '#fa436a';
+				switch(+state){
+					case 1:
+						stateTip = '待付款'; break;
+					case 2:
+						stateTip = '待使用'; break;
+					case 9:
+						stateTip = '订单已关闭'; 
+						stateTipColor = '#909399';
+						break;
+						
+					//更多自定义
+				}
+				return {stateTip, stateTipColor};
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+	page, .content{
+		background: $page-color-base;
+		height: 100%;
+	}
+	.parking-place{
+		color: red;
+		font-size: 2em
+	}
+	.swiper-box{
+		height: calc(100% - 40px);
+	}
+	.list-scroll-content{
+		height: 100%;
+	}
+	.uni-card__header-extra-text{
+	width: unset !important;
+	color: #999 !important
+	}
+	.navbar{
+		display: flex;
+		height: 40px;
+		padding: 0 5px;
+		background: #fff;
+		box-shadow: 0 1px 5px rgba(0,0,0,.06);
+		position: relative;
+		z-index: 10;
+		.nav-item{
+			flex: 1;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100%;
+			font-size: 15px;
+			color: $font-color-dark;
+			position: relative;
+			&.current{
+				color: $base-color;
+				&:after{
+					content: '';
+					position: absolute;
+					left: 50%;
+					bottom: 0;
+					transform: translateX(-50%);
+					width: 44px;
+					height: 0;
+					border-bottom: 2px solid $base-color;
+				}
+			}
+		}
+	}
+
+	.uni-swiper-item{
+		height: auto;
+	}
+	.order-item{
+		display: flex;
+		flex-direction: column;
+		background: #fff;
+		margin-top: 16upx;
+		
+		/* 多条商品 */
+		.goods-box{
+			height: 160upx;
+			padding: 20upx 0;
+			white-space: nowrap;
+			.goods-item{
+				width: 120upx;
+				height: 120upx;
+				display: inline-block;
+				margin-right: 24upx;
+			}
+			.goods-img{
+				display: block;
+				width: 100%;
+				height: 100%;
+			}
+		}
+		/* 单条商品 */
+		.goods-box-single{
+			display: flex;
+			padding: 20upx 0;
+			.goods-img{
+				display: block;
+				width: 120upx;
+				height: 120upx;
+			}
+			.right{
+				flex: 1;
+				display: flex;
+				flex-direction: column;
+				padding: 0 30upx 0 24upx;
+				overflow: hidden;
+				.title{
+					font-size: $font-base + 2upx;
+					color: $font-color-dark;
+					line-height: 1;
+				}
+				.attr-box{
+					font-size: $font-sm + 2upx;
+					color: $font-color-light;
+					padding: 10upx 12upx;
+				}
+				.price{
+					font-size: $font-base + 2upx;
+					color: $font-color-dark;
+					&:before{
+						content: '￥';
+						font-size: $font-sm;
+						margin: 0 2upx 0 8upx;
+					}
+				}
+			}
+		}
+		
+		.price-box{
+			display: flex;
+			justify-content: flex-end;
+			align-items: baseline;
+			padding-top: 20upx;
+			font-size: $font-sm + 2upx;
+			color: $font-color-light;
+			.num{
+				margin: 0 8upx;
+				color: $font-color-dark;
+			}
+			.price{
+				font-size: $font-lg;
+				color: $font-color-dark;
+				&:before{
+					content: '￥';
+					font-size: $font-sm;
+					margin: 0 2upx 0 8upx;
+				}
+			}
+		}
+		.action-box{
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+			height: 100upx;
+			position: relative;
+		}
+		.action-btn{
+			width: 160upx;
+			height: 60upx;
+			margin: 0;
+			margin-left: 24upx;
+			padding: 0;
+			text-align: center;
+			line-height: 60upx;
+			font-size: $font-sm + 2upx;
+			color: $font-color-dark;
+			background: #fff;
+			border-radius: 100px;
+			&:after{
+				border-radius: 100px;
+			}
+			&.recom{
+				background: #fff9f9;
+				color: $base-color;
+				&:after{
+					border-color: #f7bcc8;
+				}
+			}
+		}
+	}
+	
+	
+	/* load-more */
+	.uni-load-more {
+		display: flex;
+		flex-direction: row;
+		height: 80upx;
+		align-items: center;
+		justify-content: center
+	}
+	
+	.uni-load-more__text {
+		font-size: 28upx;
+		color: #999
+	}
+	
+	.uni-load-more__img {
+		height: 24px;
+		width: 24px;
+		margin-right: 10px
+	}
+	
+	.uni-load-more__img>view {
+		position: absolute
+	}
+	
+	.uni-load-more__img>view view {
+		width: 6px;
+		height: 2px;
+		border-top-left-radius: 1px;
+		border-bottom-left-radius: 1px;
+		background: #999;
+		position: absolute;
+		opacity: .2;
+		transform-origin: 50%;
+		animation: load 1.56s ease infinite
+	}
+	
+	.uni-load-more__img>view view:nth-child(1) {
+		transform: rotate(90deg);
+		top: 2px;
+		left: 9px
+	}
+	
+	.uni-load-more__img>view view:nth-child(2) {
+		transform: rotate(180deg);
+		top: 11px;
+		right: 0
+	}
+	
+	.uni-load-more__img>view view:nth-child(3) {
+		transform: rotate(270deg);
+		bottom: 2px;
+		left: 9px
+	}
+	
+	.uni-load-more__img>view view:nth-child(4) {
+		top: 11px;
+		left: 0
+	}
+	
+	.load1,
+	.load2,
+	.load3 {
+		height: 24px;
+		width: 24px
+	}
+	
+	.load2 {
+		transform: rotate(30deg)
+	}
+	
+	.load3 {
+		transform: rotate(60deg)
+	}
+	
+	.load1 view:nth-child(1) {
+		animation-delay: 0s
+	}
+	
+	.load2 view:nth-child(1) {
+		animation-delay: .13s
+	}
+	
+	.load3 view:nth-child(1) {
+		animation-delay: .26s
+	}
+	
+	.load1 view:nth-child(2) {
+		animation-delay: .39s
+	}
+	
+	.load2 view:nth-child(2) {
+		animation-delay: .52s
+	}
+	
+	.load3 view:nth-child(2) {
+		animation-delay: .65s
+	}
+	
+	.load1 view:nth-child(3) {
+		animation-delay: .78s
+	}
+	
+	.load2 view:nth-child(3) {
+		animation-delay: .91s
+	}
+	
+	.load3 view:nth-child(3) {
+		animation-delay: 1.04s
+	}
+	
+	.load1 view:nth-child(4) {
+		animation-delay: 1.17s
+	}
+	
+	.load2 view:nth-child(4) {
+		animation-delay: 1.3s
+	}
+	
+	.load3 view:nth-child(4) {
+		animation-delay: 1.43s
+	}
+	
+	@-webkit-keyframes load {
+		0% {
+			opacity: 1
+		}
+	
+		100% {
+			opacity: .2
+		}
+	}
+</style>
